e1358fdcac39c1ed6421c7e930f683a0ecf98fe9
KangTheKiss <kangthekiss@gmail.com>
2019-06-25 14:30:38 +0700

    vue-router

---
+++ vue-router/src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hello">
+    <h1>This is an home page</h1>
+    <Hello msg="This is an hello component"/>
+  </div>
+</template>
+
+<script>
+import Hello from "../components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Hello
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
